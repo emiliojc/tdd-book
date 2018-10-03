37461b3e0086c3f8053586c6d285274824ab5b91
Ontaro <emjica@gmail.com>
2018-10-03 22:24:42 +0200

    New model that items are associated with

--- lists/models.py
+++ lists/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 
-class Item(models.Model):
+class List(models.Model):
+    pass
 
+
+class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey('List', on_delete=models.SET_DEFAULT, default=None)
