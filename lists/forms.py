638e86af57d5432205bfa617bb148dd30b0d0a75
Ontaro <emjica@gmail.com>
2018-10-12 13:39:39 +0200

    Add correct duplicate validation to views and tests

--- lists/forms.py
+++ lists/forms.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ValidationError
 from lists.models import Item
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
-DUPLICATE_ITEM_ERROR = None
+DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
 
 class ItemForm(forms.models.ModelForm):
@@ -39,3 +39,6 @@ class ExistingListItemForm(ItemForm):
         except ValidationError as e:
             e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
             self._update_errors(e)
+
+    def save(self):
+        return forms.models.ModelForm.save(self)
